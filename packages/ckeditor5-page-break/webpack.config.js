4adc0bad09281339fcd805fb2bf4b6eddbe5638a
Kamil Piechaczek <pomek@users.noreply.github.com>
2020-11-25 11:09:55 +0100

    Added webpack configuration for all plugins that should be built.

---
+++ packages/ckeditor5-page-break/webpack.config.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+'use strict';
+
+/* eslint-env node */
+
+const { builds } = require( '@ckeditor/ckeditor5-dev-utils' );
+
+module.exports = builds.getPluginWebpackConfig( {
+	themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' ),
+	packagePath: __dirname,
+	mode: 'production'
+} );
